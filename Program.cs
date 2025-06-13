1b7ca95b82480d835c7b2a1707bcd21737a52fac
mqwis <mqwis@MQWIS>
2025-06-13 18:36:51 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1pk2_Sumcova1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
